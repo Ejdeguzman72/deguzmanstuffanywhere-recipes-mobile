7285eed69d92df85c3caef87275df117c6ea99ea
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-01-04 22:09:24 -0500

    initial commit

---
+++ screens/LandingScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, Text } from 'react-native';
+
+export default function LandingScreen({ navigation }) {
+    return (
+        <View style={styles.container} >
+            <Image source={require('../assets/chef.gif')} />
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity title='DSA Recipes' style={styles.button} >
+                    <Text style={styles.buttonText}>DSA Recipes</Text>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#282c34',
+        alignSelf: 'stretch',
+        color: 'white',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontFamily: 'fantasy',
+        textAlign: 'center',
+    },
+    buttonContainer: {
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: 'black',
+        alignItems: 'center',
+        borderRadius: 50,
+        height: 50,
+        justifyContent: 'center',
+        width: 250,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+        fontSize: 20
+    }
+});
